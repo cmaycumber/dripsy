45322aa2e65be8d49b05a11ae73b7786f98d1c6e
Chadwick Maycumber <chad.maycumber11@gmail.com>
2020-10-19 11:37:55 -0400

    Working on getting a working nextra example up

--- nextra/next.config.js
+++ nextra/next.config.js
@@ -1,8 +1,18 @@
-// @generated: @expo/next-adapter@2.1.9
-// Learn more: https://github.com/expo/expo/blob/master/docs/pages/guides/using-nextjs.md
+/* eslint-disable @typescript-eslint/no-var-requires */
+// @generated: @expo/next-adapter@2.1.0
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
+const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')(['dripsy'])
 const { withExpo } = require('@expo/next-adapter')
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-})
+module.exports = withPlugins([
+	withExpo(
+		withNextra(
+			withTM({
+				projectRoot: __dirname,
+			})
+		)
+	),
+])
---
+++ nextra/pages/_app.js
@@ -0,0 +1,6 @@
+/* eslint-disable react/react-in-jsx-scope */
+import 'nextra-theme-docs/style.css'
+
+export default function Nextra({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
